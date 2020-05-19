69518ba939b7bcc071ac07c14bf1e675b9287f0e
Fabio Guerreiro <ffsguerreiro@hotmail.com>
2020-05-19 10:29:34 +0100

    Day 2: first pull

--- routes/places.js
+++ routes/places.js
@@ -5,6 +5,27 @@ const placesRouter = new Router();
 const Place = require("./../models/place");
 const routeGuard = require("../middleware/route-guard");
 
+//CLOUDINARY CONFIG-------------------------------
+const multer = require("multer");
+const cloudinary = require("cloudinary");
+const multerStorageCloudinary = require("multer-storage-cloudinary");
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const storage = multerStorageCloudinary({
+  cloudinary,
+  folder: "GEOARTPROJECT"
+});
+
+const uploader = multer({ storage });
+
+//ROUTES CRUD ------------------------------------------
+
+//PLACES LIST OF A USER
 placesRouter.get("/my-list", routeGuard, (req, res, next) => {
   const creator = req.user.id;
   Place.find({ creator })
@@ -17,22 +38,26 @@ placesRouter.get("/my-list", routeGuard, (req, res, next) => {
     });
 });
 
+//CREATE NEW PLACE
 placesRouter.get("/create", routeGuard, (req, res, next) => {
   res.render("places/create");
 });
 
-placesRouter.post("/create", routeGuard, (req, res, next) => {
+placesRouter.post("/create", routeGuard, uploader.single("picture"), (req, res, next) => {
   const name = req.body.name;
   const description = req.body.description;
   const latitude = req.body.latitude;
   const longitude = req.body.longitude;
+  const pictureUrl = req.file.url;
   console.log(req.user.id);
+
   return Place.create({
     name,
     description,
     location: {
       coordinates: [longitude, latitude]
     },
+    pictureUrl,
     creator: req.user.id
   })
     .then((place) => {
@@ -43,6 +68,7 @@ placesRouter.post("/create", routeGuard, (req, res, next) => {
     });
 });
 
+//SINGLE PLACE VIEW
 placesRouter.get("/:id", (req, res, next) => {
   const id = req.params.id;
   Place.findById(id)
@@ -54,6 +80,7 @@ placesRouter.get("/:id", (req, res, next) => {
     });
 });
 
+//UPDATE PLACE
 placesRouter.get("/update/:id", (req, res, next) => {
   const id = req.params.id;
 
@@ -82,6 +109,19 @@ placesRouter.post("/update/:id", (req, res, next) => {
   };
 
   Place.findOneAndUpdate({ _id: id }, query)
+    .then((place) => {
+      res.redirect("/places/my-list");
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+//DELETE PLACE
+placesRouter.post("/delete/:id", (req, res, next) => {
+  const id = req.params.id;
+
+  Place.findByIdAndDelete(id)
     .then((place) => {
       res.redirect("/places/my-list");
     })
