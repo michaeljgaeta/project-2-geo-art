e491ac930648781781eb679fc548f4f654391f71
michaeljgaeta <michaeljgaeta@gmail.com>
2020-05-19 10:28:33 +0100

    day 2, 10:30AM

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,46 @@
 "use strict";
 
 const { Router } = require("express");
-const router = new Router();
 const routeGuard = require("./../middleware/route-guard");
 
+//require place model
+const Place = require("./../models/place");
+
+//require cloudinary packages
+const multer = require("multer");
+const cloudinary = require("cloudinary");
+const multerStorageCloudinary = require("multer-storage-cloudinary");
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const storage = multerStorageCloudinary({
+  cloudinary,
+  folder: "april-2020"
+});
+
+const uploader = multer({ storage });
+// const uploader = multer({ dest: 'tmp' }); // Storing locally in tmp folder
+
+const router = new Router();
+
+//get place from database, display by most recent
 router.get("/", (req, res, next) => {
-  res.render("index", { title: "Geo Art" });
+  Place.find()
+    .sort({ createdAt: -1 })
+    // .limit(2)
+    // .skip(1)
+    .populate("creator")
+    .then((places) => {
+      console.log(places);
+      res.render("index", { places });
+    })
+    .catch((error) => {
+      next(error);
+    });
 });
 
 router.get("/private", routeGuard, (req, res, next) => {
